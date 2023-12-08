cbc5e882c98f0b4e0228189a3ab72c3eb5179fc2
Madu.i <pathiranamadushan@gmail.com>
2023-12-08 18:13:35 +0530

    config and wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+""" cacl module"""
+
+def add(x,y):
+    """ Add x and y"""
+    return x+y
+
+def substract(x,y):
+    """substract x from y """
+    
+    return y-x
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#c#!q)r%x$xu^t0)%n8vae!ys9+4iw%_o*rxw#0#@soyg@c5u'
+SECRET_KEY = 'django-insecure-56fnf2=hs0k-22c*c4o=ao)tp+nv$&z#p6-9to-e9&jy+jku4w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'ENGINE':'django.db.backends.postgresql',
+     'HOST':os.environ.get('DB_HOST'),
+     'NAME':os.environ.get('DB_NAME'),
+     'USER':os.environ.get('DB_USER'),
+     'PASSWORD':os.environ.get('DB_PASS'),
+     
     }
 }
 
